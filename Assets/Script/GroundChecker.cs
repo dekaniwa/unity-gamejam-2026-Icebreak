fae80f01fc8b1a9b016dddfd8432fb67ab09c8cc
大庭稜生 <ca01264007@st.kawahara.ac.jp>
2026-07-22 15:29:55 +0900

    地面判定用スクリプト

--- Assets/Script/GroundChecker.cs
+++ Assets/Script/GroundChecker.cs
@@ -1,16 +1,30 @@
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundChec : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 接地しているかを格納する変数
+    bool m_isGround;
+    // 地面に触れているかを返す関数
+    public bool GetIsGround()
     {
-        
+        return m_isGround;
     }
 
-    // Update is called once per frame
-    void Update()
+    // 毎フレーム最初に接地判定をリセットする
+    private void FixedUpdate()
     {
-        
+        m_isGround = false;
+    }
+    // 自身に何かが衝突している間呼ばれる
+    private void OnTriggerStay(Collider other)
+    {
+        // 地面のタグが付いたオブジェクトに衝突している
+        if (other.CompareTag("Ground"))
+        {
+            m_isGround = true;
+        }
     }
 }
